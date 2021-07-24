8d201cc629d83cb34d208825f4392b428bae8ab7
shon-arch <sunnywahane14@gmail.com>
2021-07-24 20:19:33 +0530

    Completed the logic for drawing colors

--- grid.js
+++ grid.js
@@ -2,13 +2,36 @@ const div = document.querySelector(".root-div");
 
 const colorPicker = document.querySelector("input"); 
 
-let x=4; 
+let currentColor = ["black"]; 
+
+let x=5; 
 
 createGrid(); 
+
+
 document.querySelector("#clear").addEventListener("click",(e)=>{
     div.textContent=''; 
-    x=prompt("Enter the height of the grid"); 
     createGrid()
+})
+
+document.querySelector("#size").addEventListener("change",(e)=>{
+    x=e.target.value; 
+    div.textContent=''; 
+    createGrid(); 
+})
+
+document.querySelector("#erase").addEventListener("click",(e)=>{
+            currentColor=["white"]; 
+})
+
+document.querySelector("#draw").addEventListener("click",(e)=>{
+    currentColor=["black"]; 
+})
+
+document.querySelector("#random").addEventListener("click",(e)=>{
+    let colors = ["#fff100 ","#ff8c00 ","#e81123","#ec008c ","#68217a ","#00188f ","#00bcf2 ",
+                    "#00b294 ","#009e49 "]; 
+    currentColor = colors; 
 })
 
 function  createGrid(){
@@ -18,11 +41,11 @@ for(let j=0;j<x;++j){
     pdiv.classList.add("parent-div"); 
     for(let i=0;i<x; ++i){
         let newDiv = document.createElement("div"); 
-        let newpad = (300)/x ; 
+        let newpad = Math.floor((280)/x); 
         newDiv.style.padding = newpad+"px"; 
         newDiv.classList.add("div-color")
         newDiv.addEventListener("mouseenter",(e)=>{
-            newDiv.classList.toggle("color-changer"); 
+            newDiv.style["background-color"] = currentColor[Math.floor(Math.random()*currentColor.length)]; 
         }) 
         pdiv.appendChild(newDiv); 
         
